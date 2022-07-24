2dacc7ac0f0d088845b79a460bcd6e4da8129d4c
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-24 09:46:15 +0530

    created navbar and the home-page

---
+++ frontend/src/components/layout/Landing.jsx
@@ -0,0 +1,53 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+import image from "../../assets/7747.jpg";
+import easy from "../../assets/easy-to-use.png";
+import userin from "../../assets/user-interface.png";
+import responsive from "../../assets/responsive.png"
+
+const Landing = () => {
+  return (
+    <Fragment>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col">
+            <h1 className="display-1">
+              <strong>QNA</strong>
+            </h1>
+            <h2 className="h2">Perfect place to find answer</h2>
+            <p class="lead">
+              Create a QNA account and ask your questions from someone who knows
+              the answer
+            </p>
+            <Link to="/register" className="btn btn-primary btn-lg">
+              REGISTER
+            </Link>{" "}
+            <Link to="/login" className="btn btn-dark btn-lg">
+              LOGIN
+            </Link>
+          </div>
+          <div class="col">
+            <img src={image} alt="Image" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+      <div className="container text-center mt-5">
+        <h2 className="display-4">Why QNA ?</h2>
+        <p className="lead">
+          Why you should consider using QNA ? Seriously Why ?
+        </p>
+        <div class="row align-items-center">
+          <div class="col">
+            <h4>Easy To Use</h4>
+            <img src={easy} width="200" alt="Image" className="rounded img-thumbnail" />
+          </div>
+          <div class="col"><h4>Bette user interface</h4><img src={userin} width="200" alt="Image" className="rounded img-thumbnail" /></div>
+          <div class="col"><h4>Responsive design</h4><img src={responsive} width="200" alt="Image" className="rounded img-thumbnail" /></div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Landing;
---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import image from "../../assets/qna_logo.png"
+
+const Navbar = () => {  
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+          <Link class="navbar-brand" to="/">
+              <img src={image} alt="" width="55" height="50" />
+          </Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Register</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Login</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+//     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//   <div class="container-fluid">
+//     <a class="navbar-brand" href="#">Navbar</a>
+//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//       <span class="navbar-toggler-icon"></span>
+//     </button>
+//     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+//         <li class="nav-item">
+//           <a class="nav-link active" aria-current="page" href="#">Home</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">Link</a>
+//         </li>
+//         <li class="nav-item dropdown">
+//           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Dropdown
+//           </a>
+//           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+//             <li><a class="dropdown-item" href="#">Action</a></li>
+//             <li><a class="dropdown-item" href="#">Another action</a></li>
+//             <li><hr class="dropdown-divider" /></li>
+//             <li><a class="dropdown-item" href="#">Something else here</a></li>
+//           </ul>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+    );
+  };
+  
+export default Navbar;

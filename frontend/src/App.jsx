ecd2dfbc000b946e36f3b75612ea04bf64c423cc
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-24 11:21:50 +0530

    added auth and footer components to the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }

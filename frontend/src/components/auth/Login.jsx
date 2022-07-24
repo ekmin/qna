6597f496ccc526d552fc57b8e7d32693f2d394d4
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-24 18:04:31 +0530

    made login and register work

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,32 +1,104 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
-import image from "../../assets/login.jpg"
+import image from "../../assets/login.jpg";
+
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../../store/reducers/auth.reducers";
+import api from "../../utils/api";
 
 const Login = () => {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const body = { email, password };
+
+    try {
+      const res = await api.post("/auth", body);
+
+      dispatch(authActions.LOGIN_SUCCESS(res.data));
+
+      try {
+        const res = await api.get("/auth");
+
+        dispatch(authActions.USER_LOADED(res.data));
+      } catch (err) {
+        console.log(err);
+        dispatch(authActions.AUTH_ERROR());
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => setError(error.msg));
+      }
+
+      dispatch(authActions.LOGIN_FAIL());
+    }
+  };
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
   return (
-      <div class="container mt-4" style={{marginBottom: "80px"}}>
-        <div class="row align-items-center">
-          <div class="col-md">
+    <div className="container mt-4" style={{ marginBottom: "80px" }}>
+      <div className="row align-items-center">
+        <div className="col-md">
           <img src={image} alt="Image" className="img-fluid" />
-          <Link to="/register"><p className="lead text-center">Create an account</p></Link>
-          </div>
-          <div class="col-md">
-            <h1 className="h1 mb-3 text-primary">Login</h1>
-            <form>
-            <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingEmail" placeholder="your email" />
-                <label for="floatingEmail">Your email</label>
+          <Link to="/register">
+            <p className="lead text-center">Create an account</p>
+          </Link>
+        </div>
+        <div className="col-md">
+          <h1 className="h1 mb-3 text-primary">Login</h1>
+          <form onSubmit={submitHandler}>
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={onChange}
+                className="form-control"
+                id="floatingEmail"
+                placeholder="your email"
+              />
+              <label htmlFor="floatingEmail">Your email</label>
             </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="password" />
-                <label for="floatingPassword">Password</label>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+                className="form-control"
+                id="floatingPassword"
+                placeholder="password"
+              />
+              <label htmlFor="floatingPassword">Password</label>
             </div>
-            <button type="submit" className="btn btn-dark btn-lg">Login</button>
-            </form>
-          </div>
+            <button type="submit" className="btn btn-dark btn-lg">
+              Login
+            </button>
+          </form>
         </div>
       </div>
+    </div>
   );
 };
 
--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,40 +1,136 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import image from "../../assets/computer.jpg"
+import image from "../../assets/computer.jpg";
+
+import { authActions } from "../../store/reducers/auth.reducers";
+import setAuthToken from "../../utils/setAuthToken";
+import api from "../../utils/api";
 
 const Register = () => {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    if (password !== password2) {
+      setError("Passwords Do Not Match");
+    } else {
+      try {
+        const res = await api.post("/register", formData);
+
+        dispatch(authActions.REGISTER_SUCCESS(res.data));
+
+        if (localStorage.token) {
+          setAuthToken(localStorage.token);
+        }
+        try {
+          const res = await api.get("/auth");
+
+          dispatch(authActions.USER_LOADED(res.data));
+        } catch (err) {
+          dispatch(authActions.AUTH_ERROR());
+        }
+      } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+          errors.forEach((error) => setError(error.msg));
+          console.log(errors);
+        }
+
+        dispatch(authActions.AUTH_ERROR());
+      }
+    }
+  }
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
   return (
-      <div class="container mt-4">
-        <div class="row align-items-center">
-          <div class="col-md">
-            <h1 className="h1 mb-3">Register</h1>
-            <form>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingName" placeholder="your name" />
-                <label for="floatingName">Your name</label>
+    <div className="container mt-4">
+      <div className="row align-items-center">
+        <div className="col-md">
+          <h1 className="h1 mb-3">Register</h1>
+          <form onSubmit={submitHandler}>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={onChange}
+                className="form-control"
+                id="floatingName"
+                placeholder="your name"
+              />
+              <label htmlFor="floatingName">Your name</label>
             </div>
-            <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingEmail" placeholder="your email" />
-                <label for="floatingEmail">Your email</label>
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={onChange}
+                className="form-control"
+                id="floatingEmail"
+                placeholder="your email"
+              />
+              <label htmlFor="floatingEmail">Your email</label>
             </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="password" />
-                <label for="floatingPassword">Password</label>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={onChange}
+                className="form-control"
+                id="floatingPassword"
+                placeholder="password"
+              />
+              <label htmlFor="floatingPassword">Password</label>
             </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingConPassword" placeholder="confirm password" />
-                <label for="floatingConPassword">Confirm password</label>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                name="password2"
+                value={password2}
+                onChange={onChange}
+                className="form-control"
+                id="floatingConPassword"
+                placeholder="confirm password"
+              />
+              <label htmlFor="floatingConPassword">Confirm password</label>
             </div>
-            <button type="submit" className="btn btn-primary btn-lg">Register</button>
-            </form>
-          </div>
-          <div class="col-md">
+            <button type="submit" className="btn btn-primary btn-lg">
+              Register
+            </button>
+          </form>
+        </div>
+        <div className="col-md">
           <img src={image} alt="Image" className="img-fluid" />
-          <Link to="/login"><p className="lead text-center">I am already a member</p></Link>
-          </div>
+          <Link to="/login">
+            <p className="lead text-center">I am already a member</p>
+          </Link>
         </div>
       </div>
+    </div>
   );
 };
 

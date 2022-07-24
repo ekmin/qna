ae0e5b7e5f5ac424c8194ebd77577cc0eb2b2d88
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-24 11:20:47 +0530

    created footer component

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer class="footer bg-dark bg-opacity-75 w-100 py-3  bottom-0 start-50 translate-middle-x fixed-bottom">
+    <div class="container text-center">
+      <span class="lead text-light">Made For Hashnode PlanetScale Hackathon</span>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer

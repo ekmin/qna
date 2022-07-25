b7c8dd2ff721f7a9a1ae618c99468ba46e30fd44
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-25 20:48:24 +0530

    added alert.jsx to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
@@ -8,9 +8,29 @@ import Footer from "./components/layout/Footer";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+import Alert from "./components/layout/Alert";
+
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./store/actions/auth.actions";
+
 function App() {
+  const alert = useSelector((state) => state.alert.alert);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+      dispatch(loadUser());
+    }
+
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) dispatch(authActions.LOGOUT());
+    });
+  }, []);
+
   return (
     <Router>
+      {alert?.message && <Alert message={alert.message} type={alert.type} />}
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />

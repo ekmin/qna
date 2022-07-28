820df8aa2ae17f8bec242dae7b1bed249f4e2579
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-28 15:27:00 +0530

    fixed to top

--- frontend/src/components/layout/Alert.jsx
+++ frontend/src/components/layout/Alert.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Alert(props) {
   return (
-    <div className={`alert alert-${props.type}`} role="alert">
-        {props.message}
+    <div className={`alert alert-${props.alert.type} fixed-top`} role="alert">
+        {props.alert.message}
     </div>
   )
 }

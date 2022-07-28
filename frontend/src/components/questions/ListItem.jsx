bc57cc676f30e2b2a49d8a0d09ff1ef21ed330dd
Ekmin Samaraweera <96468923+ekmin@users.noreply.github.com>
2022-07-28 16:56:41 +0530

    added listitem

--- frontend/src/components/questions/ListItem.jsx
+++ frontend/src/components/questions/ListItem.jsx
@@ -3,24 +3,27 @@ import { Link } from "react-router-dom";
 
 const ListItem = (props) => {
   return (
-    // <div className="profile bg-light">
-    //   <div>
-    //     <h2>{props.que_name}</h2>
-    //     <p>{props.date} at</p>
-    //     <Link to={`/question/${props.id}`} className="btn btn-primary">
-    //       View Picnic
-    //     </Link>
-    //   </div>
-    // </div>
-<div class="card mb-3">
-  <div class="card-body">
-  <h5 class="card-title"><Link to={`/question/${props.id}`} className="text-decoration-none">{props.que_name}</Link></h5>
-    <p>{props.creator_name}</p>
-    <blockquote class="blockquote mb-0">
-    <footer class="blockquote-footer">{props.edited ? `Last edited on ${props.date}` : `Created on ${props.date}`}</footer>
-    </blockquote>
-  </div>
-</div>
+    <div className="card mb-3">
+      <div className="card-body">
+        <h5 className="card-title">
+          <Link to={`/question/${props.id}`} className="text-decoration-none">
+            {props.que_name}
+          </Link>
+        </h5>
+        <div class="row">
+          <div class="col-sm">
+            <p>
+              {props.edited
+                ? `Last edited on ${props.date}`
+                : `Created on ${props.date}`}
+            </p>
+          </div>
+          <div class="col">
+            <footer className="blockquote-footer">{props.creator_name}</footer>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
